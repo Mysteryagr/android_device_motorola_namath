dcab1a00958ae8fb71e88b9a22618cff65481f7d
Mysteryagr <ahmedrahmy842@gmail.com>
2017-12-06 20:14:36 +0000

    Bring up ViperOS branch

--- viper.mk
+++ viper.mk
@@ -3,8 +3,8 @@
 # Release name
 PRODUCT_RELEASE_NAME := namath
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common ViperOS stuff.
+$(call inherit-product, vendor/viper/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/motorola/namath/device_namath.mk)
@@ -15,7 +15,7 @@ DEVICE_RESOLUTION := 480x854
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := namath
-PRODUCT_NAME := lineage_namath
+PRODUCT_NAME := viper_namath
 PRODUCT_BRAND := Motorola
 PRODUCT_MODEL := Moto C
 PRODUCT_MANUFACTURER := Motorola
