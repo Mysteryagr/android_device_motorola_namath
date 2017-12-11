02216eeb94adfd602718883c38cbc4c508b85499
Mysteryagr <ahmedrahmy842@gmail.com>
2017-12-11 15:32:26 +0000

    Bring up DotOS branch

--- dot.mk
+++ dot.mk
@@ -3,8 +3,8 @@
 # Release name
 PRODUCT_RELEASE_NAME := namath
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common DotOS stuff.
+$(call inherit-product, vendor/dot/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/motorola/namath/device_namath.mk)
@@ -15,7 +15,7 @@ DEVICE_RESOLUTION := 480x854
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := namath
-PRODUCT_NAME := lineage_namath
+PRODUCT_NAME := dot_namath
 PRODUCT_BRAND := Motorola
 PRODUCT_MODEL := Moto C
 PRODUCT_MANUFACTURER := Motorola
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,4 @@
 # Use this to add P1m to CM's lunch command menu
 for var in eng user userdebug; do
-  add_lunch_combo lineage_namath-$var
+  add_lunch_combo dot_namath-$var
 done
