b5e479aed08c9390d22480d13987252daf15cfde
Mysteryagr <ahmedrahmy842@gmail.com>
2017-12-22 23:21:08 +0000

    Remove other assert names

--- device_namath.mk
+++ device_namath.mk
@@ -186,7 +186,7 @@ PRODUCT_PACKAGES += \
     
 # Health HAL
 PRODUCT_PACKAGES += \
-    android.hardware.health@1.0-impl \
+#    android.hardware.health@1.0-impl \
     android.hardware.health@1.0-service
     
     
