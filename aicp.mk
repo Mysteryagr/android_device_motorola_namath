571b3a47a309ec3b15a0d9973580d03b01f63674
Mysteryagr <ahmedrahmy842@gmail.com>
2017-12-22 17:25:54 +0000

    Bringup AICP branch

--- aicp.mk
+++ aicp.mk
@@ -3,8 +3,8 @@
 # Release name
 PRODUCT_RELEASE_NAME := namath
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common aicp stuff.
+$(call inherit-product, vendor/aicp/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/motorola/namath/device_namath.mk)
